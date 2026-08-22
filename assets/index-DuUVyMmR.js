f1c8c603e7169bc08699085c7ffc4f7f7eb1d7ce
pavelhalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-08-22 21:25:55 +0300

    Updates

---
+++ assets/index-DuUVyMmR.js
@@ -0,0 +1,17 @@
+(function(){let e=document.createElement(`link`).relList;if(e&&e.supports&&e.supports(`modulepreload`))return;for(let e of document.querySelectorAll(`link[rel="modulepreload"]`))n(e);new MutationObserver(e=>{for(let t of e)if(t.type===`childList`)for(let e of t.addedNodes)e.tagName===`LINK`&&e.rel===`modulepreload`&&n(e)}).observe(document,{childList:!0,subtree:!0});function t(e){let t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),t.credentials=e.crossOrigin===`use-credentials`?`include`:e.crossOrigin===`anonymous`?`omit`:`same-origin`,t}function n(e){if(e.ep)return;e.ep=!0;let n=t(e);fetch(e.href,n)}})();var e={async get(){let e=await fetch(`http://localhost:3000/garage/`),t=e.status;if(t!==200)throw Error(`HTTP ${t}`);return await e.json()},async create(e){let t=await fetch(`http://localhost:3000/garage/`,{method:`POST`,body:JSON.stringify(e),headers:{"Content-Type":`application/json`}}),n=t.status;if(n!==201)throw Error(`HTTP ${n}`);return await t.json()}},t=`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free 7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M147 170.7L117.2 256L240.1 256L240.1 160L162.2 160C155.4 160 149.3 164.3 147.1 170.7zM48.6 257.9L86.5 149.6C97.8 117.5 128.1 96 162.1 96L360 96C385.2 96 408.9 107.9 424 128L520.2 256.3C587.1 260.5 640 316.1 640 384L640 400C640 435.3 611.3 464 576 464L559.6 464C555.6 508.9 517.9 544 472 544C426.1 544 388.4 508.9 384.4 464L239.7 464C235.7 508.9 198 544 152.1 544C106.2 544 68.5 508.9 64.5 464L64.1 464C28.8 464 .1 435.3 .1 400L.1 320C.1 289.9 20.8 264.7 48.7 257.9zM440 256L372.8 166.4C369.8 162.4 365 160 360 160L288 160L288 256L440 256zM152 496C174.1 496 192 478.1 192 456C192 433.9 174.1 416 152 416C129.9 416 112 433.9 112 456C112 478.1 129.9 496 152 496zM512 456C512 433.9 494.1 416 472 416C449.9 416 432 433.9 432 456C432 478.1 449.9 496 472 496C494.1 496 512 478.1 512 456z"/></svg>`,n={render(e){return`<span class="car__wrapper" style="color: ${e};">${t.replaceAll(/fill="[^"]*"/g,``).replace(`<svg`,`<svg fill="currentColor"`)}</span>`}},r={async render(){return(await e.get()).map(e=>`
+        <div>
+          ${e.id} ${e.name}
+        </div>
+        <div>
+          ${n.render(e.color)}
+        </div>
+      `).join(``)}},i={idForm:`create_car_form`,render(){return`
+      <form id="${i.idForm}">
+        <input type="text" name="name">
+        <input type="color" name="color">
+        <button>Create</button>
+      </form>
+    `},init(){let e=document.querySelector(`#${i.idForm}`);if(!e){console.error(`Node not found: #${i.idForm}`);return}e.addEventListener(`submit`,i.onSubmit)},async onSubmit(t){t.preventDefault();let n=t.target,r=new FormData(n),i={name:r.get(`name`)||`Unnamed car`,color:r.get(`color`)||`#000000`};console.log(`Try to create car`,i);let a=await e.create(i);console.log(`Created car`,a)}};try{document.querySelector(`#app`).innerHTML=`
+    ${i.render()}
+    ${await r.render()}
+  `,i.init()}catch(e){console.error(e)}
