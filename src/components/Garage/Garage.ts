8a373361eb1cdaa8c19cddb821f9db04fdc307c9
pavelhalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-08-22 19:42:41 +0300

    feat: car component added

--- src/components/Garage/Garage.ts
+++ src/components/Garage/Garage.ts
@@ -1,11 +1,19 @@
 import GarageApi from '../../api/garage/GarageApi';
+import Car from '../Car/Car';
 
 const Garage = {
   async render(): Promise<string> {
     const GARAGE = await GarageApi.get();
 
     return GARAGE.map(car => {
-      return `<div style="color: ${car.color};">${car.id} ${car.name}</div>`;
+      return `
+        <div>
+          ${car.id} ${car.name}
+        </div>
+        <div>
+          ${Car.render(car.color)}
+        </div>
+      `;
     }).join('');
   },
 };
